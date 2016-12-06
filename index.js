3ecba684e290024f2ae243f9a372784093b382e1
Damian Krzeminski <pirxpilot@code42day.com>
2016-12-05 16:20:30 -0800

    area-calc

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ var heap = require('code42day-binary-heap');
 
 module.exports = simplify;
 
-function area(t) {
+function area(t, index) {
+  var
+    a = t[index],
+    b = t[index - 1],
+    c = t[index + 1];
+
   return Math.abs(
-    (t[0][0] - t[2][0]) * (t[1][1] - t[0][1]) -
-    (t[0][0] - t[1][0]) * (t[2][1] - t[0][1])
+    (a[0] - c[0]) * (b[1] - a[1]) -
+    (a[0] - b[0]) * (c[1] - a[1])
   );
 }
 
@@ -20,12 +25,14 @@ function calculate(poly) {
     heap: heap(areaCompare),
     list: []
   };
+  var ta;
 
   // calculate areas
   for (i = 1; i < poly.length - 1; i++) {
-    triangle = poly.slice(i - 1, i + 2);
-    triangle.area = area(triangle);
-    if (triangle.area) {
+    ta = area(poly, i);
+    if (ta) {
+      triangle = poly.slice(i - 1, i + 2);
+      triangle.area = ta;
       ts.list.push(triangle);
     }
   }
@@ -56,14 +63,14 @@ function eliminate(ts, limit) {
     if (triangle.prev) {
       triangle.prev.next = triangle.next;
       triangle.prev[2] = triangle[2];
-      triangle.prev.area = area(triangle.prev);
+      triangle.prev.area = area(triangle.prev, 1);
     } else {
       ts.first = triangle.next;
     }
     if (triangle.next) {
       triangle.next.prev = triangle.prev;
       triangle.next[0] = triangle[0];
-      triangle.next.area = area(triangle.next);
+      triangle.next.area = area(triangle.next, 1);
     }
     // some areas have changed - need to adjust the heap
     ts.heap.rebuild();
